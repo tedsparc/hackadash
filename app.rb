e9cfe70258be197ddf74cc1e86cbe07426561711
Ted Behling <ted@tedb.us>
2012-08-20 15:32:46 -0400

    moved HAML to view file; restructed app to change Github data model; added setup script for Github organization

--- app.rb
+++ app.rb
@@ -16,117 +16,176 @@ $config = {
   websocket_port: 3003,
   websocket_url_hostname: 'tedb.us',
   github_oauth_token: '24ce7b9d6c039ea6210cf754ef40295f702baf81',
-  github_org: 'ted-hackathon-test'
+#  github_org: 'ted-hackathon-test'
+  github_org: 'sparc-hackathon-2-0'
 }
 
 class GithubWebHook < Hashie::Mash
-  def nbr_added
-    self.commits.reduce(0) { |sum, this_commit| sum + this_commit.added.length }
-  end
+end
 
-  def nbr_modified
-    self.commits.reduce(0) { |sum, this_commit| sum + this_commit.modified.length }
-  end
-
-  def nbr_removed
-    self.commits.reduce(0) { |sum, this_commit| sum + this_commit.removed.length }
-  end
-  
-  def nbr_total
-    nbr_added + nbr_modified + nbr_removed
-  end
-  
-  def graph_item
-    [ self.repository.name, self.nbr_total ]
+class Time
+  # Convert time to milliseconds since UTC epoch, for use by Javascript
+  def to_utc_ms
+    self.utc.to_i * 1000
   end
 end
 
-class Series
-  # This utility class method is used in this class as well as in GithubWebHook
-  def Series.get_github_commit_stats(repo_name, sha)
-    warn "get details for repo: #{repo_name} sha: #{sha}"
+class SeriesSet
+  attr_accessor :github_org, :github_handle, :series_set
+  
+  def initialize(github_org, github_oauth_token)
+    @series_set = []
+    @github_org = github_org
+    @github_handle = Github.new oauth_token: github_oauth_token
     
-    commit_details = $github.repos.commits.get($config[:github_org], repo_name, sha)
-    
-    commit_timestamp = Time.iso8601(commit_details.commit.author['date']).utc
-    commit_net_lines_added = commit_details.stats.additions - commit_details.stats.deletions
-    
-    return commit_timestamp, commit_net_lines_added
+    fetch_repo_list.sort_by { |r| r.name }.each do |this_repo|
+      @series_set << RepoDataSeries.new(self, this_repo.name)
+    end
   end
   
-  # Return array of strings, containing series names
+  def fetch_repo_list
+    self.github_handle.repos.list(org: self.github_org, per_page: 100)
+  end
+  
   def series_names
-    $github.repos.list(org: $config[:github_org], per_page: 100).map &:name
-    #('team01'..'team05').to_a
+    self.series_set.map(&:name).sort
   end
   
-  # Returns the data set for one series.
-  # We actually display the date in local time, but the data is provided to the browser as UTC, as ms since UTC epoch
-  def data_for_series(repo_name)
-    $github.repos.commits.list($config[:github_org], repo_name).sort_by { |this_commit|
-      Time.iso8601(this_commit.commit.author['date']).to_i
-    }.reduce([]) { |agg, this_commit|
-      # Get the commit details
-      commit_timestamp, commit_net_lines_added = Series.get_github_commit_stats(repo_name, this_commit.sha)
-
-      # Do our math to calculate the net lines added in this commit
-      previous_total_lines = agg[-1][1] rescue 0
-      commit_total_lines = previous_total_lines + commit_net_lines_added
-      
-      # Emit a data point from our reduce method
-      # Note: Javascript's "new Date()" call wants milliseconds since Unix epoch, in UTC
-      agg << [ commit_timestamp.to_i * 1000, commit_total_lines ]
-      puts agg.inspect
-      agg
-    }
+  def [](query_repo_name)
+    self.series_set.select { |repo| repo.name == query_repo_name }.first
   end
   
-  # useful for development
-  def random_data_for_series(repo_name)
-    [
-      [(Time.now.utc - rand(5000)).to_i * 1000, rand(100)],
-      [(Time.now.utc - rand(5000)).to_i * 1000, rand(100)],
-      [(Time.now.utc - rand(5000)).to_i * 1000, rand(100)],
-      [(Time.now.utc - rand(5000)).to_i * 1000, rand(100)]
-    ].sort_by { |x| x[0] }
-  end
-  
-  def all_series_data
+  # Returns data ready for injesting into Highcharts
+  def all_data
     self.series_names.map do |this_name|
-      [this_name, data_for_series(this_name)]
+      [this_name, self[this_name].data_series]
+    end
+  end
+  
+  # Hand off the Github web hook data to whichever RepoDataSeries is responsible
+  def handle_github_web_hook(github_post_data)
+    self[github_post_data.repository.name].handle_github_web_hook(github_post_data)
+  end
+  
+  class RepoDataSeries
+    attr_accessor :parent, :name, :data_series, :latest_broadcast
+    
+    def initialize(parent, repo_name)
+      @parent = parent
+      @name = repo_name
+      @data_series = data_for_series
+    end
+    
+    def github
+      self.parent.github_handle
+    end
+    
+    def fetch_commits_for_repo
+      begin
+        self.github.repos.commits.list(self.parent.github_org, self.name).sort_by { |this_commit|
+          # Just used for sorting
+          Time.iso8601(this_commit.commit.author['date']).to_i
+        }
+      rescue Github::Error::ServiceError => e
+        warn "Github error in RepoDataSeries#fetch_commits_for_repo: #{e}"
+        return []
+      end
+    end
+    
+    # This utility class method is used in this class as well as in GithubWebHook
+    def fetch_stats_for_commit(sha)
+      warn "get details for repo: #{self.name} sha: #{sha}"
+    
+      commit_details = self.github.repos.commits.get(self.parent.github_org, self.name, sha)
+    
+      commit_timestamp = Time.iso8601(commit_details.commit.author['date']).utc
+      warn "stats: #{commit_details.stats.to_json}"
+      commit_net_lines_added = commit_details.stats.additions - commit_details.stats.deletions
+    
+      return commit_timestamp, commit_net_lines_added
+    end
+  
+    # Returns the data set for one series; only used upon object initialization
+    # We actually display the date in local time, but the data is provided to the browser as UTC, as milliseconds since UTC epoch
+    def data_for_series
+      now_ms = Time.now.to_utc_ms
+      data = fetch_commits_for_repo.reduce([]) { |agg, this_commit|
+        # Get the commit details
+        commit_timestamp, commit_net_lines_added = fetch_stats_for_commit(this_commit.sha)
+
+        # Do our math to calculate the net lines added in this commit
+        previous_total_lines = agg[-1][1] rescue 0
+        commit_total_lines = previous_total_lines + commit_net_lines_added
+      
+        # Emit a data point from our reduce method
+        # Note: Javascript's "new Date()" call wants milliseconds since Unix epoch, in UTC
+        agg << [ commit_timestamp.to_utc_ms, commit_total_lines ]
+        puts agg.inspect
+        agg
+      }
+      # If there are no Github commits, make a fake entry so we get something on the chart
+      return data.empty? ? [[now_ms, 1]] : data
+    end
+    
+    # injest a new Github web hook object
+    # look up the commit using the Github API
+    # Return data suitable for sending to Websocket clients for injest by Highcharts
+    def handle_github_web_hook(github_post_data)
+      self.latest_broadcast = []
+      
+      github_post_data.commits.each do |this_commit|
+        warn "Handling Github commit notification for repo #{self.name}: #{this_commit.id}"
+        commit_timestamp, commit_net_lines_added = fetch_stats_for_commit(this_commit.id)
+        data_point = [ commit_timestamp.to_utc_ms, data_series[-1][1] + commit_net_lines_added ]
+        
+        warn "data point: #{data_point.to_json}"
+        self.data_series << data_point
+        self.latest_broadcast << [self.name, data_point]
+      end
+    
+      return self.latest_broadcast
+    end
+      
+    # useful for development
+    def random_data_for_series()
+      [
+        [(Time.now.utc - rand(5000)).to_utc_ms, rand(100)],
+        [(Time.now.utc - rand(5000)).to_utc_ms, rand(100)],
+        [(Time.now.utc - rand(5000)).to_utc_ms, rand(100)],
+        [(Time.now.utc - rand(5000)).to_utc_ms, rand(100)]
+      ].sort_by { |x| x[0] }
     end
   end
 end
 
-$series = Series.new
-$github = Github.new oauth_token: $config[:github_oauth_token]
+$series_set = SeriesSet.new($config[:github_org], $config[:github_oauth_token])
 $sockets = []
 
 EventMachine.run do
   class App < Sinatra::Base
     get '/' do
-      haml :websocket
+      haml :index
     end
     
     get '/all.json' do
       content_type 'application/json'
-      $series.all_series_data.to_json
+      $series_set.all_data.to_json
     end
     
     post '/github' do
       warn "from github: #{params[:payload]}"
-      payload = GithubWebHook.new JSON.parse(params[:payload])
+      payload = $series_set.handle_github_web_hook GithubWebHook.new JSON.parse(params[:payload])
+      warn "sending payload #{payload.to_json}"
       #debugger
-      warn "+%s -%s /%s" % [ payload.nbr_added, payload.nbr_removed, payload.nbr_modified ]
       
-      $sockets.each { |s| s.send payload.graph_item.to_json }
-      ''
+      $sockets.each { |s| s.send payload.to_json }
+      'OK'
     end
     
     post '/pushdebug' do
       # e.g. curl -4 -v -d 'payload=["team01", [1344823587000, 42]]' tedb.us:3002/pushdebug
       $sockets.each { |s| s.send params[:payload] }
-      ''
+      'OK'
     end
     
     enable :inline_templates
@@ -154,97 +213,3 @@ EventMachine.run do
 end
 
 __END__
-
-@@websocket
-
-!!!
-%head
-  %title Hackadash: SPARC Hackathon 2.0!
-  %script(src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
-  %script(src="/highcharts.js")
-  
-  :javascript
-    var socket = new WebSocket('ws://#{ $config[:websocket_url_hostname] }:#{ $config[:websocket_port] }');
-
-    socket.onopen = function (e) {
-      $('#notifications').append("<li>-- WebSocket onopen!</li>");
-    };
-
-    socket.onmessage = function (e) {
-      $('#notifications').append("<li>From server: " + e.data + "</li>");
-      server_point = JSON.parse(e.data);
-      series = chart1.get('series_' + server_point[0]);
-      series.addPoint(server_point[1]);
-    };
-
-    socket.onclose = function(e) {
-      $('#notifications').append("<li>-- WebSocket onclose!</li>");
-    };
-    
-    function initial_chart_data() {
-      $.ajax({
-        url: 'all.json',
-        cache: false,
-        // resulting JSON is automatically parsed
-        success: function(server_series_set) {
-          
-          server_series_set.forEach(function(this_server_series) {
-            // Add this series to the chart, without redrawing the chart
-            chart1.addSeries({
-              id: 'series_' + this_server_series[0],
-              name: this_server_series[0],
-              data: this_server_series[1]
-            }, false);
-          })
-
-          chart1.redraw();
-          
-        }
-      });
-    }
-    
-    var chart1; // globally available
-    $(document).ready(function() {
-      Highcharts.setOptions({
-        global: {
-          useUTC: false
-        }
-      });
-      
-      chart1 = new Highcharts.Chart({
-         chart: {
-            renderTo: 'chart_container',
-            type: 'line',
-            zoomType: 'x',
-            events: {
-              load: initial_chart_data
-            }
-         },
-         credits: {
-           enabled: false
-         },
-         title: {
-            text: 'Git repository code volume by team'
-         },
-         xAxis: {
-            type: 'datetime',
-            tickPanelInterval: 150,
-            title: {
-              text: 'Time'
-            }
-         },
-         yAxis: {
-            min: 0,
-            title: {
-               text: 'Total lines of code'
-            }
-         },
-         series: []
-      });
-    });
-%body    
-  #chart_container(style="width: 100%; height: 400px")
-  
-  / Received Websocket messages will be inserted into this list
-  %ul#notifications
-  
---
+++ setup-hackathon-repos.rb
@@ -0,0 +1,62 @@
+require 'github_api'
+require 'em-synchrony'
+
+$ORG = 'sparc-hackathon-2-0'
+$WEBHOOK_URL = 'http://tedb.us:3002/github'
+
+# to get an oauth token:   curl -u tedsparc -d '{"scopes": ["repo", "user"], "note":"dev test for hackadash, 2"}' https://api.github.com/authorizations
+$github = Github.new oauth_token: 'fbab5220d341e89c8612f25ab01720946932dfaa', adapter: :em_synchrony
+
+case ARGV[0]
+when 'setup'
+  puts "Setting up repos for #{$ORG}..."
+  ("team01".."team20").each do |this_team|
+    begin                    
+      warn "Creating repo for #{this_team}..."
+      $github.repos.create name: this_team,
+                          org: $ORG,
+                          description: "SPARC Hackathon 2.0: #{this_team}",
+                          private: false,
+                          homepage: "http://hackathon.sparcedge.com", 
+                          has_wiki: false,
+                          has_downloads: false,
+                          has_issues: false
+       
+      warn "Creating team #{this_team}..."
+      $github.orgs.teams.create $ORG, name: this_team, permission: "push", repo_names: [ "#{$ORG}/#{this_team}"]
+                   
+      warn "Creating repo hook for #{this_team}..."
+      $github.repos.hooks.create $ORG, this_team, name: "web",
+                                                 active: true,
+                                                 config: { url: $WEBHOOK_URL }
+    rescue Github::Error::UnprocessableEntity => e
+      warn "Github error for #{this_team} => #{e}"
+    end
+  end
+else
+  puts "Listing repos for #{$ORG}..."
+  $github.repos.list(org: $ORG, per_page: 100) do |this_repo|
+    puts "Repo: " + this_repo.name
+    
+    teams = $github.repos.teams($ORG, this_repo.name)
+    # FIXME: display list of teams associated with this repo; display team members
+  end
+end
+
+
+__END__
+github.orgs.list do |org|
+  next unless org.login == 
+  puts org.login
+  
+  github.repos.list(user: org.login, per_page: 100) do |repo|
+    puts "  " + repo.name
+    
+    github.repos.commits.list(org.login, repo.name) do |commit|
+      puts "    " + commit.sha
+      details = github.repos.commits.get(org.login, repo.name, commit.sha)
+      puts "      author date: " + details.commit.author['date']
+      puts "      net added: " + (details.stats.additions - details.stats.deletions).to_s
+    end
+  end
+end
